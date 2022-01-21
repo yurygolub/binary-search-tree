fe1e7f1f71a21c7eb9d86a8542bdbdd82613f679
Yura <yuragolub21@gmail.com>
2022-01-21 18:23:07 +0300

    Add unit tests.

--- BinarySearchTreeTask.Tests/BinarySearchTreeTests.cs
+++ BinarySearchTreeTask.Tests/BinarySearchTreeTests.cs
@@ -1,10 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
 using NUnit.Framework;
 
 namespace BinarySearchTreeTask.Tests
 {
-    [TestFixture]
-    public class BinarySearchTreeTests
+    [TestFixtureSource(typeof(TestFixtures), nameof(TestFixtures.Fixtures))]
+    public class BinarySearchTreeTests<T>
     {
-        //TODO: Add test methods here. Remove this comment.
+        private readonly BinarySearchTree<T> tree;
+        private readonly T[] array;
+        private readonly T value;
+        private readonly int initCount;
+        private readonly IComparer<T> comparer;
+
+        public BinarySearchTreeTests(T[] source, T value, int count, IComparer<T> comparer)
+        {
+            this.tree = new BinarySearchTree<T>(source, comparer);
+            this.value = value;
+            this.initCount = count;
+            this.array = source;
+            this.comparer = comparer;
+        }
+
+        [Test]
+        public void Ctor_BasedOnComparer()
+        {
+            var tree = new BinarySearchTree<T>(this.comparer);
+            tree.Add(this.value);
+            CollectionAssert.Contains(tree, this.value);
+        }
+
+        [Test]
+        [Order(0)]
+        public void Ctor_BasedOnEnumerableSource()
+        {
+            Assert.That(this.tree.Count == this.initCount);
+        }
+
+        [Test]
+        [Order(1)]
+        public void DefaultIterator_Test()
+        {
+            int i = 0;
+            T[] sortedArray = (T[])this.array.Clone();
+            Array.Sort(sortedArray, this.comparer);
+            foreach (T item in this.tree)
+            {
+                Assert.AreEqual(item, sortedArray[i++]);
+            }
+        }
+
+        [Test]
+        [Order(2)]
+        public void Add_OneElement()
+        {
+            int count = this.tree.Count;
+            this.tree.Add(this.value);
+            Assert.Multiple(() =>
+            {
+                CollectionAssert.Contains(this.tree, this.value);
+                Assert.That(this.tree.Count == count + 1);
+            });
+        }
+
+        [Test]
+        [Order(3)]
+        public void Contains_Test()
+        {
+            Assert.IsTrue(this.tree.Contains(this.value));
+        }
+
+        [Test]
+        [Order(4)]
+        public void Remove_OneElement()
+        {
+            int count = this.tree.Count;
+            this.tree.Remove(this.value);
+            Assert.Multiple(() =>
+            {
+                CollectionAssert.DoesNotContain(this.tree, this.value);
+                Assert.That(this.tree.Count == count - 1);
+            });
+        }
+
+        [Test]
+        [Order(5)]
+        public void Remove_ItemIsNotExist_ThrowInvalidOperationException()
+        {
+            Assert.Throws<InvalidOperationException>(() => this.tree.Remove(this.value), "Invalid operation remove, item is not exist.");
+        }
+
+        [Test]
+        [Order(6)]
+        public void Clear_Test()
+        {
+            this.tree.Clear();
+            Assert.Multiple(() =>
+            {
+                CollectionAssert.IsEmpty(this.tree);
+                Assert.That(this.tree.Count == 0);
+            });
+        }
+
+        [Test]
+        [Order(7)]
+        public void Remove_TreeIsEmpty_ThrowInvalidOperationException()
+        {
+            Assert.Throws<InvalidOperationException>(() => this.tree.Remove(this.value), "Invalid operation remove, tree is empty.");
+        }
     }
 }
---
+++ BinarySearchTreeTask.Tests/TestFixtures.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace BinarySearchTreeTask.Tests
+{
+    public class TestFixtures
+    {
+        public static IEnumerable<TestFixtureData> Fixtures
+        {
+            get
+            {
+                yield return new TestFixtureData(typeof(int), new[] { 12, 3, 4, int.MinValue, -12, int.MaxValue, 45, 12 }, 67, 8, Comparer<int>.Default);
+                yield return new TestFixtureData(typeof(int), new[] { 12, 3, 4, int.MinValue, -12, int.MaxValue, 45, 12 }, 67, 8, new IntComparer());
+                yield return new TestFixtureData(typeof(string), new[] { "12", "Zero", "Test", "Hello" }, "Hello, world!", 4, Comparer<string>.Default);
+                yield return new TestFixtureData(typeof(string), new[] { "12", "Zero", "Test", "Hello" }, "Hello, world!", 4, new StringComparer());
+                
+                yield return new TestFixtureData(typeof(Book),
+                    new[]
+                    {
+                        new Book("author", "title", "publisher", "3-598-21508-8"),
+                        new Book("abc", "", "", ""),
+                        new Book("fgdg", "titleqwe", "asdsf", "3-598-21507-X"),
+                    },
+                    new Book("Jon Skeet", "C# in Depth", "Manning Publications", "978-0-901-69066-1"), 3, Comparer<Book>.Default);
+
+                yield return new TestFixtureData(typeof(Book),
+                    new[]
+                    {
+                        new Book("author", "title", "publisher", "3-598-21508-8"),
+                        new Book("abc", "", "", ""),
+                        new Book("fgdg", "titleqwe", "asdsf", "3-598-21507-X"),
+                    },
+                    new Book("Jon Skeet", "C# in Depth", "Manning Publications", "978-0-901-69066-1"), 3, new BookComparer());
+
+                yield return new TestFixtureData(typeof(Time),
+                    new[]
+                    {
+                        new Time(10, 30),
+                        new Time(16, 34),
+                        new Time(18, 53),
+                        new Time(0, 0),
+                    },
+                    new Time(23, 5), 4, new TimeComparer());
+            }
+        }
+    }
+}
